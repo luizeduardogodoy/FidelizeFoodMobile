e61909210704a880d79e4abfabe0c4a3e11ca5e8
Luiz Eduardo <luizeduardogodoy@gmail.com>
2018-03-23 17:37:26 -0300

    layout cliente x restaurante

--- app/src/main/java/com/example/luizeduardo/fidelizefood/FidelizeMain.java
+++ app/src/main/java/com/example/luizeduardo/fidelizefood/FidelizeMain.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,21 @@ public class FidelizeMain extends AppCompatActivity {
         String nomeUser = bundle.getString("nomeUser");
 
         textView.setText(nomeUser + " - tipo " + tipo);
+
+
+        LinearLayout cliente = findViewById(R.id.layoutCliente);
+        LinearLayout restaurante = findViewById(R.id.layoutRest);
+
+        restaurante.setVisibility(View.GONE);
+        cliente.setVisibility(View.GONE);
+
+        if(tipo == 2){
+            restaurante.setVisibility(View.VISIBLE);
+        }
+        else{
+            cliente.setVisibility(View.VISIBLE);
+        }
+
 
         Toast.makeText(this,nomeUser,Toast.LENGTH_LONG).show();
 
