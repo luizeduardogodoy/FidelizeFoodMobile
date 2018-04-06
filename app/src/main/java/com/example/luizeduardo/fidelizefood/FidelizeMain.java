f6e6c975ab20aa5451a09c797338a605059151d4
Luiz Eduardo <luizeduardogodoy@gmail.com>
2018-04-06 17:54:36 -0300

    remove imports não utilizados

--- app/src/main/java/com/example/luizeduardo/fidelizefood/FidelizeMain.java
+++ app/src/main/java/com/example/luizeduardo/fidelizefood/FidelizeMain.java
@@ -1,22 +1,16 @@
 package com.example.luizeduardo.fidelizefood;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
