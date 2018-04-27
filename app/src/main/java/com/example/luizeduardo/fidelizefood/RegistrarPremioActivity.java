e7b054925479e566e98a4c6c2a27fc8a0acd2773
Luiz Eduardo <luizeduardogodoy@gmail.com>
2018-04-27 17:29:24 -0300

    funcionalidade de premio

--- app/src/main/java/com/example/luizeduardo/fidelizefood/RegistrarPremioActivity.java
+++ app/src/main/java/com/example/luizeduardo/fidelizefood/RegistrarPremioActivity.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RegistrarPremioActivity extends AppCompatActivity {
 
 
@@ -30,8 +35,8 @@ public class RegistrarPremioActivity extends AppCompatActivity {
         }
         else{
 
-            String post = "req=premi0";
-
+            String post = "req=premi0&idusercliente="+ textView.getText().toString();
+            post += "&UsuarioID=" + UserSingleton.getInstance().getUser().getId();
 
 
             new RegistrarPremio().execute(ConnectAPITask.urlAPI, post);
@@ -44,6 +49,42 @@ public class RegistrarPremioActivity extends AppCompatActivity {
 
 
     class RegistrarPremio extends ConnectAPITask{
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
 
+            JSONObject json =  null;
+
+            try {
+                json = new JSONObject(s);
+
+                if (json.getString("status").equals("ok")) {
+
+
+                    Utils.alertInfo(RegistrarPremioActivity.this,
+                            "Fidelize - Prêmio",
+                            json.getString("nome") + " - " + json.getString("mensagem"));
+
+                    if(json.has("idnotificacao")) {
+
+                        String userId = json.getString("idnotificacao");
+                        try {
+                            OneSignal.postNotification(new JSONObject(
+                                    "{'contents': " +
+                                            "{'en':'Você atingiu seu cartão de fidelidade. Restaurante: "  +  UserSingleton.getInstance().getUser().getNome() +  " '}, " +
+                                            "'include_player_ids': ['" + userId + "']," +
+                                            "'app_id': 'c72fac2d-99ae-4623-a530-68f252d9399d'}"), null);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
